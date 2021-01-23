968a1061c419836af06b142ab2c700e2a4d35a02
Justinnn07 <justinv2006@gmail.com>
2021-01-23 20:10:58 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="insta-logo">
+        <img
+          src="https://instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+      <div className="search">
+        <input type="text" placeholder="🔍 Search" />
+      </div>
+      <div className="btns">
+        <button className="blue">LOGIN</button>
+        <button className="transparent">Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
